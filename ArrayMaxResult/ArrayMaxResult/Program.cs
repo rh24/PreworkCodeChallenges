054e15685e1c7b24bb1bcc5a8441c9daafba3b8f
rebeccahong <rebeccayhong@gmail.com>
2018-10-03 23:07:28 -0400

    Fix things in error list

--- ArrayMaxResult/ArrayMaxResult/Program.cs
+++ ArrayMaxResult/ArrayMaxResult/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace ArrayMaxResult
 {
@@ -6,12 +8,49 @@ namespace ArrayMaxResult
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[] { 1, 2, 2, 4, 5 };
+            //Console.WriteLine("Hello World!");
+            Console.Write("result: " + CalculateMax(numbers));
+            Console.ReadLine();
         }
 
-        public int ArrayMaxResult(int[] args)
+        static int CalculateMax(int[] args)
         {
+            int[] numbers = args;
+            int tempMaxCount;
+            int tempMaxNum;
+            Hashtable ht = new Hashtable();
 
+            foreach (int i in numbers)
+            {
+                if (ht.Contains(i))
+                {
+                    int oldVal = (int)ht[i];
+                    ht[i] = oldVal + 1;
+                }
+                else
+                {
+                    ht.Add(i, 1);
+                }
+            }
+
+            int currMaxCount = (int)ht[numbers[0]];
+            int currMaxNum = numbers[0];
+
+            for (int i = 0; i < numbers.Distinct<int>().Count(); i++)
+            {
+                currMaxCount = (int)ht[numbers[i]];
+                tempMaxCount = (int)ht[numbers[i + 1]];
+                currMaxNum = numbers[i];
+                tempMaxNum = numbers[i + 1];
+
+                if (tempMaxCount > currMaxCount)
+                {
+                    currMaxNum = tempMaxNum;
+                }
+            }
+
+            return currMaxNum * currMaxCount;
         }
     }
 }
