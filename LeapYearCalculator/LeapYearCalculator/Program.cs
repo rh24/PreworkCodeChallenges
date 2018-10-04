90e199626631a8b6c9c2d682e38dab5d6520e977
rebeccahong <rebeccayhong@gmail.com>
2018-10-04 00:52:36 -0400

    Try again at stuff bc why not

--- LeapYearCalculator/LeapYearCalculator/Program.cs
+++ LeapYearCalculator/LeapYearCalculator/Program.cs
@@ -6,9 +6,30 @@ namespace LeapYearCalculator
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Leap Year Calculator!");
             Console.WriteLine(IsLeapYear(1996));
+            Console.WriteLine(IsLeapYear(1997));
+            Console.WriteLine(IsLeapYear(2000));
+            Console.WriteLine(IsLeapYear(1900));
+            
+            Console.WriteLine(ArrayMaxResult());
             Console.ReadLine();
+            
+        }
+
+        static int ArrayMaxResult()
+        {
+            int[] numbers = new int[5];
+
+            Console.WriteLine("Please input a number and press enter until further instructions.");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Your array is [{0}]", string.Join(", ", numbers));
+            Console.ReadLine();
+            return 0;
         }
 
         static bool IsLeapYear(int num)
