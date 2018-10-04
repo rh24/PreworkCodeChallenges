c8436c5a2e3655f7c9f3be263af0534a44337d55
rebeccahong <rebeccayhong@gmail.com>
2018-10-03 21:20:41 -0400

    Add Problem 1 project

---
+++ ArrayMaxResult/ArrayMaxResult/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArrayMaxResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int ArrayMaxResult(int[] args)
+        {
+
+        }
+    }
+}
